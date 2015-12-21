1924adf92d46b43b4889c592d59950b0a849cd34
alexandregama <alexandre.gama.lima@gmail.com>
2015-12-21 20:47:59 -0200

    Adds some comments to Functional Interface

--- src/test/java/br/com/mastering/java/defaultmethod/Formula.java
+++ src/test/java/br/com/mastering/java/defaultmethod/Formula.java
@@ -1,8 +1,14 @@
 package br.com.mastering.java.defaultmethod;
 
+//The compiler will throws a compiler error if we have more than one abstract method here
+@FunctionalInterface
 public interface Formula {
 
-	double calculate(int a);
+	double calculate(int a); //we can have just one abstract method
+	
+	default double anotherDefaultMethod(int a) {
+		return a;
+	}
 	
 	default double sqrt(int a) {
 		return Math.sqrt(a);

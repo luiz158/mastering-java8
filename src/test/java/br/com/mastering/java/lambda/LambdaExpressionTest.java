d9a9f77f02fd2b93fdde6e72c2a579ef550874b3
alexandregama <alexandre.gama.lima@gmail.com>
2015-12-21 18:38:33 -0200

    Adds a method to sort list name using lambda expression but in a long way

--- src/test/java/br/com/mastering/java/lambda/LambdaExpressionTest.java
+++ src/test/java/br/com/mastering/java/lambda/LambdaExpressionTest.java
@@ -1,6 +1,6 @@
 package br.com.mastering.java.lambda;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,6 +21,20 @@ public class LambdaExpressionTest {
 			public int compare(String s1, String s2) {
 				return s1.compareTo(s2);
 			}
+		});
+		
+		assertEquals("Bruna", names.get(0));
+		assertEquals("Carlos", names.get(1));
+		assertEquals("Gama", names.get(2));
+		assertEquals("Gustavo", names.get(3));
+	}
+	
+	@Test
+	public void shouldSortStringsUsingLambdaExpressionInALongWay() throws Exception {
+		List<String> names = Arrays.asList("Gama", "Bruna", "Carlos", "Gustavo");
+		
+		Collections.sort(names, (String s1, String s2) -> {
+			return s1.compareTo(s2);
 		});
 		
 		assertEquals("Bruna", names.get(0));
